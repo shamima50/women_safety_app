f96a0102500ceb42b44570aa6abf29809f7bd449
anjum44 <anjumafrin303@gmail.com>
2024-06-27 03:14:17 +0600

    test 7

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:women_safety_app/home_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;
   runApp(const MyApp());
 }
 
