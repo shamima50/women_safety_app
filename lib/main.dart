49d805b1076d386ea77aa9010945c0f770f87317
anjum44 <anjumafrin303@gmail.com>
2024-07-07 01:23:33 +0600

    test 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:women_safety_app/firebase_options.dart';
 import 'package:women_safety_app/home_screen.dart';
 
 void main() async{
+  /*WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;*/
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp;
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
