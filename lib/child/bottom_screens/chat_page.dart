9502e9390692dcf76efae6dc446e5aaf6f814f5b
ZarinTasnimRitu <tasnimritu906@gmail.com>
2024-07-11 02:26:49 +0600

    chhh32

---
+++ lib/child/bottom_screens/chat_page.dart
@@ -0,0 +1,132 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:women_safety_app/chat_module/chat_screen.dart';
+import 'package:women_safety_app/child/child_login_screen.dart';
+import 'package:women_safety_app/db/share_pref.dart';
+import 'package:women_safety_app/parent/parent_home_screen.dart';
+
+import '../../utils/constants.dart';
+
+class CheckUserStatusBeforeChat extends StatelessWidget {
+  const CheckUserStatusBeforeChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else {
+          if (snapshot.hasData) {
+            print("===>${snapshot.data}");
+            return StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("users")
+                  .where("id",
+                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                  .snapshots(),
+              builder: (context, snap) {
+                if (snap.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snap.hasData) {
+                  if (snap.data!.docs.first.data()['type'] == "parent") {
+                    return ParentHomeScreen();
+                  } else {
+                    return ChatPage();
+                  }
+                }
+                return SizedBox();
+              },
+            );
+            //return ChatPage();
+          } else {
+            Fluttertoast.showToast(msg: 'please login first');
+            return LoginScreen();
+          }
+        }
+      },
+    );
+  }
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   WidgetsBinding.instance.addPostFrameCallback((_) {
+  //     setState(() {
+  //       if (FirebaseAuth.instance.currentUser == null ||
+  //           FirebaseAuth.instance.currentUser!.uid.isEmpty) {
+  //         if (mounted) {
+  //           Navigator.push(
+  //               context, MaterialPageRoute(builder: (_) => LoginScreen()));
+  //         }
+  //       }
+  //     });
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // WidgetsBinding.instance.addObserver();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        // backgroundColor: Color.fromARGB(255, 250, 163, 192),
+        title: Text("SELECT GUARDIAN"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('type', isEqualTo: 'parent')
+            .where('childEmail',
+            isEqualTo: FirebaseAuth.instance.currentUser!.email)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: progressIndicator(context));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final d = snapshot.data!.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Color.fromARGB(255, 250, 163, 192),
+                  child: ListTile(
+                    onTap: () {
+                      goTo(
+                          context,
+                          ChatScreen(
+                              currentUserId:
+                              FirebaseAuth.instance.currentUser!.uid,
+                              friendId: d.id,
+                              friendName: d['name']));
+                      // Navigator.push(context, MaterialPa)
+                    },
+                    title: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(d['name']),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/child/bottom_screens/profile_page.dart
@@ -0,0 +1,243 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
+import 'package:women_safety_app/child/bottom_page.dart';
+import 'package:women_safety_app/child/child_login_screen.dart';
+import 'package:women_safety_app/components/PrimaryButton.dart';
+import 'package:women_safety_app/components/custom_textfield.dart';
+import 'package:women_safety_app/utils/constants.dart';
+
+class CheckUserStatusBeforeChatOnProfile extends StatelessWidget {
+  const CheckUserStatusBeforeChatOnProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else {
+          if (snapshot.hasData) {
+            return ProfilePage();
+          } else {
+            Fluttertoast.showToast(msg: 'please login first');
+            return LoginScreen();
+          }
+        }
+      },
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameC = TextEditingController();
+  TextEditingController guardianEmailC = TextEditingController();
+  TextEditingController childEmailC = TextEditingController();
+  TextEditingController phoneC = TextEditingController();
+
+  final key = GlobalKey<FormState>();
+  String? id;
+  String? profilePic;
+  String? downloadUrl;
+  bool isSaving = false;
+  getDate() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where('id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((value) {
+      setState(() {
+        nameC.text = value.docs.first['name'];
+        childEmailC.text = value.docs.first['childEmail'];
+        guardianEmailC.text = value.docs.first['guardiantEmail'];
+        phoneC.text = value.docs.first['phone'];
+        id = value.docs.first.id;
+        profilePic = value.docs.first['profilePic'];
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: isSaving == true
+          ? Center(
+          child: CircularProgressIndicator(
+            backgroundColor: Colors.pink,
+          ))
+          : SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Center(
+            child: Form(
+                key: key,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "UPDATE YOUR PROFILE",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    SizedBox(height: 15),
+                    GestureDetector(
+                      onTap: () async {
+                        final XFile? pickImage = await ImagePicker()
+                            .pickImage(
+                            source: ImageSource.gallery,
+                            imageQuality: 50);
+                        if (pickImage != null) {
+                          setState(() {
+                            profilePic = pickImage.path;
+                          });
+                        }
+                      },
+                      child: Container(
+                        child: profilePic == null
+                            ? CircleAvatar(
+                          backgroundColor: Colors.deepPurple,
+                          radius: 80,
+                          child: Center(
+                              child: Image.asset(
+                                'assets/add_pic.png',
+                                height: 80,
+                                width: 80,
+                              )),
+                        )
+                            : profilePic!.contains('http')
+                            ? CircleAvatar(
+                          backgroundColor: Colors.deepPurple,
+                          radius: 80,
+                          backgroundImage:
+                          NetworkImage(profilePic!),
+                        )
+                            : CircleAvatar(
+                            backgroundColor: Colors.deepPurple,
+                            radius: 80,
+                            backgroundImage:
+                            FileImage(File(profilePic!))),
+                      ),
+                    ),
+                    CustomTextField(
+                      controller: nameC,
+                      hintText: nameC.text,
+                      validate: (v) {
+                        if (v!.isEmpty) {
+                          return 'please enter your updated name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: childEmailC,
+                      hintText: "child email",
+                      readOnly: true,
+                      validate: (v) {
+                        if (v!.isEmpty) {
+                          return 'please enter your updated name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: guardianEmailC,
+                      hintText: "parent email",
+                      readOnly: true,
+                      validate: (v) {
+                        if (v!.isEmpty) {
+                          return 'please enter your updated name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: phoneC,
+                      hintText: "Phone number",
+                      readOnly: true,
+                      validate: (v) {
+                        if (v!.isEmpty) {
+                          return 'please enter your updated name';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 25),
+                    PrimaryButton(
+                        title: "UPDATE",
+                        onPressed: () async {
+                          if (key.currentState!.validate()) {
+                            SystemChannels.textInput
+                                .invokeMethod('TextInput.hide');
+                            profilePic == null
+                                ? Fluttertoast.showToast(
+                                msg: 'please select profile picture')
+                                : update();
+                          }
+                        })
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<String?> uploadImage(String filePath) async {
+    try {
+      final filenName = Uuid().v4();
+      final Reference fbStorage =
+      FirebaseStorage.instance.ref('profile').child(filenName);
+      final UploadTask uploadTask = fbStorage.putFile(File(filePath));
+      await uploadTask.then((p0) async {
+        downloadUrl = await fbStorage.getDownloadURL();
+      });
+      return downloadUrl;
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
+
+  update() async {
+    setState(() {
+      isSaving = true;
+    });
+    uploadImage(profilePic!).then((value) {
+      Map<String, dynamic> data = {
+        'name': nameC.text,
+        'profilePic': downloadUrl,
+      };
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .update(data);
+      setState(() {
+        isSaving = false;
+        goTo(context, BottomPage());
+      });
+    });
+  }
+}
