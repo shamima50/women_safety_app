d10f0f20ec0e914beb77b18bf6b03adf5957b195
anjum44 <anjumafrin303@gmail.com>
2024-07-31 21:13:51 +0600

    test 14

--- lib/child/bottom_screens/chat_page.dart
+++ lib/child/bottom_screens/chat_page.dart
@@ -5,7 +5,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:women_safety_app/chat_module/chat_screen.dart';
 import 'package:women_safety_app/child/child_login_screen.dart';
 import 'package:women_safety_app/db/share_pref.dart';
-import 'package:women_safety_app/parent/parent_home_screen.dart';
+import 'package:women_safety_app/parents/parent_home_screen.dart';
 
 import '../../utils/constants.dart';
 
